f0312eb51f54a98ec10a4cf2f07a65fbdd9ffbdd
Kamiwey <kamiwey@gmail.com>
2023-07-26 19:05:36 +0200

    delete project working

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from 'src/app/services/project.service';
+import { Global } from 'src/app/services/global';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
+  providers: [ProjectService]
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit{
+  public url: string;
+  public project: Project;
+
+  constructor(
+    private _projectService: ProjectService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ){ 
+    this.url = Global.url;
+    this.project = new Project('', '', '', '', 2019, '', '');
+  
+  }
+
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      let id = params['id'];
+
+      this.getProject(id);
+    });
+  }
+
+  getProject(id: any){
+    this._projectService.getProject(id).subscribe(
+      response => {
+        this.project = response.project;
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
+  }
+
+  deleteProject(id: any){
+    this._projectService.deleteProject(id).subscribe(
+      response => {
+        if(response.project){
+          this._router.navigate(['/proyectos']);
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
+  }
 
 }
